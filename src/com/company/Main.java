b281702796fda0db5cb729ee729755fd8e838510
LazarenkoDmytro <lazarenkodmytro.work@gmail.com>
2024-09-20 17:31:41 +0300

    2nd

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,10 +11,22 @@ public class Main {
         System.out.println(x * x + y * y <= 16 ? "Inside" : "Outside");
     }
 
+    private static void isInsideTriangle(Scanner scanner) {
+        System.out.print("Enter the coordinates of the point: ");
+        double x = scanner.nextDouble();
+        double y = scanner.nextDouble();
+
+        if (y <= x && 6 * y >= x && 2 * y <= -3 * x + 20) {
+            System.out.println("Inside");
+        } else {
+            System.out.println("Outside");
+        }
+    }
+
     public static void main(String[] args) {
-        isInsideCircle();
         try (Scanner scanner = new Scanner(System.in)) {
             isInsideCircle(scanner);
+            isInsideTriangle(scanner);
         }
     }
 }
