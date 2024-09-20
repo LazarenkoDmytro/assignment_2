3e80e5a102ecda89ccac241ab5097e9657afb778
LazarenkoDmytro <lazarenkodmytro.work@gmail.com>
2024-09-20 17:31:28 +0300

    1st edited

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,18 +3,18 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
-    private static void isInsideCircle() {
-        try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print("Enter the coordinates of the point: ");
+    private static void isInsideCircle(Scanner scanner) {
+        System.out.print("Enter the coordinates of the point: ");
+        double x = scanner.nextDouble();
+        double y = scanner.nextDouble();
 
-            int x = scanner.nextInt();
-            int y = scanner.nextInt();
-
-            System.out.println(x * x + y * y <= 16 ? "Inside" : "Outside");
-        }
+        System.out.println(x * x + y * y <= 16 ? "Inside" : "Outside");
     }
 
     public static void main(String[] args) {
         isInsideCircle();
+        try (Scanner scanner = new Scanner(System.in)) {
+            isInsideCircle(scanner);
+        }
     }
 }
